a22e9d7215d9659e16d38dff9444275c80da127e
bryan-learn <blearn@psc.edu>
2016-12-14 14:59:50 -0500

    added cli & module version of script to lookup coordinates by IP in geolite2 db; added script to update the geolite2 db

---
+++ src/geolocate/ip2geo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys, getopt
+import geoip2.database
+import json
+
+def lookupIP(ip):
+    _db = "GeoLite2-City.mmdb"
+    _ip = ip
+    _format = "geojson" #["text", "geojson"]
+
+    ## Search Database for IP ##
+
+    reader = geoip2.database.Reader(_db) 			# Load database
+    res = reader.city(_ip)					# Query db for _ip
+    coords = [res.location.longitude, res.location.latitude]	# Validate result coords then store
+
+    if(_format == "text"):
+        print coords
+    if(_format == "geojson"):
+        gjson = {"type": "Point", "coordinates": coords}
+        print(json.dumps(gjson))
+
