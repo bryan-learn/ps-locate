b9ef60967ba194f9a82bef3823ce8346aa19b852
bryan-learn <blearn@psc.edu>
2017-02-06 12:04:27 -0500

    updated verison number and the update_perfsonar_data script.

--- update_perfsonar_data.sh
+++ update_perfsonar_data.sh
@@ -2,17 +2,5 @@
 
 # Using relative paths. Only run this script from root directory of the project (where it lives).
 
-cd ./ps-data
-# download perfSONAR records from simple_lookup_service
-python download_all_records.py
+python ./ps_data/ps_data.py --update
 
-# store perfSONAR records in local mongoDB
-python load_records_to_mongo.py
-
-cd ./geolocate
-# lookup the geo coordinates for all available IP addresses in perfSONAR data
-python populate_ps_geoip.py
-
-cd ./ps-data
-# Previous steps updated the 'staging' database. Now we can remove old data and rotate 'staging' to 'ps-data'
-python rotate_dbs.py
