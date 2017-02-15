20ac9b0c951d0e15aee231f1d69f70eadd1ec1e7
bryan-learn <blearn@psc.edu>
2017-02-15 13:05:09 -0500

    Added missing step to the update script. (geoip indexing was erroneously excluded)

--- update_perfsonar_data.sh
+++ update_perfsonar_data.sh
@@ -2,5 +2,15 @@
 
 # Using relative paths. Only run this script from root directory of the project (where it lives).
 
-python ./ps_data/ps_data.py --update
+# download perfSONAR records
+python ./ps_data/ps_data.py --download
+
+# store perfSONAR records into local mongodb instance for 'staging'
+python ./ps_data/ps_data.py --store
+
+# process the 'staging' data to create geoip indexing
+python ./geolocate/populate_ps_geoip.py
+
+# copy 'staging' database to production database, 'ps-data'
+python ./ps_data/ps_data.py --rotate
 
