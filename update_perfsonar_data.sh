309caa2394f8f35d5bdb6e56421c9f9dc1e3a77d
bryan-learn <blearn@psc.edu>
2017-02-17 11:16:20 -0500

    Added error checking to updater_perfsonar_data.sh script.

--- update_perfsonar_data.sh
+++ update_perfsonar_data.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
 # Using relative paths. Only run this script from root directory of the project (where it lives).
+echo "Begin perfSONAR DB update.."
 
+cd ./ps_data
 # download perfSONAR records
-python ./ps_data/ps_data.py --download
+if python ./ps_data.py --download; then
+	echo "    download: Success"
+else
+	echo "    download: Failed"
+	exit 1
+fi
 
 # store perfSONAR records into local mongodb instance for 'staging'
-python ./ps_data/ps_data.py --store
+if python ./ps_data.py --store; then
+	echo "    store: Success"
+else
+	echo "    store: Failed"
+	exit 1
+fi
 
+cd ../geolocate
 # process the 'staging' data to create geoip indexing
-python ./geolocate/populate_ps_geoip.py
+if python ./populate_ps_geoip.py; then
+	echo "    geoip indexing: Success"
+else
+	echo "    geoip indexing: Failed"
+	exit 1
+fi
 
+cd ../ps_data
 # copy 'staging' database to production database, 'ps-data'
-python ./ps_data/ps_data.py --rotate
+if python ./ps_data.py --rotate; then
+	echo "    rotate: Success"
+else
+	echo "    rotate: Failed"
+	exit 1
+fi
+
+echo "perfSONAR DB update: Success"
 
