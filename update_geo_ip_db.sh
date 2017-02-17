9ca7a5cbc6ed84673e2e1717bd7aa3fd3d0fe626
bryan-learn <blearn@psc.edu>
2017-02-17 11:25:05 -0500

    Included date timestamps in update output

--- update_geo_ip_db.sh
+++ update_geo_ip_db.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "[$(date)] Begin GeoLite2 IP DB  update.."
 cd ./geolocate
 wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
 gzip -d GeoLite2-City.mmdb.gz
+if [ "$?" = "0" ]; then
+	echo "[$(date)] update success"
+else
+	echo "[$(date)] update failed"
+	exit 1
+fi
--- update_perfsonar_data.sh
+++ update_perfsonar_data.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 # Using relative paths. Only run this script from root directory of the project (where it lives).
-echo "Begin perfSONAR DB update.."
+echo "[$(date)] Begin perfSONAR DB update.."
 
 cd ./ps_data
 # download perfSONAR records
 if python ./ps_data.py --download; then
-	echo "    download: Success"
+	echo "    [$(date)] download: Success"
 else
-	echo "    download: Failed"
+	echo "    [$(date)] download: Failed"
 	exit 1
 fi
 
 # store perfSONAR records into local mongodb instance for 'staging'
 if python ./ps_data.py --store; then
-	echo "    store: Success"
+	echo "    [$(date)] store: Success"
 else
-	echo "    store: Failed"
+	echo "    [$(date)] store: Failed"
 	exit 1
 fi
 
 cd ../geolocate
 # process the 'staging' data to create geoip indexing
 if python ./populate_ps_geoip.py; then
-	echo "    geoip indexing: Success"
+	echo "    [$(date)] geoip indexing: Success"
 else
-	echo "    geoip indexing: Failed"
+	echo "    [$(date)] geoip indexing: Failed"
 	exit 1
 fi
 
 cd ../ps_data
 # copy 'staging' database to production database, 'ps-data'
 if python ./ps_data.py --rotate; then
-	echo "    rotate: Success"
+	echo "    [$(date)] rotate: Success"
 else
-	echo "    rotate: Failed"
+	echo "    [$(date)] rotate: Failed"
 	exit 1
 fi
 
-echo "perfSONAR DB update: Success"
+echo "[$(date)] perfSONAR DB update: Success"
 
